43fb799dbceb917667151f808f3d94221036eea9
Nenadan <ristivojevicnenad@gmail.com>
2024-10-09 11:53:03 +0200

    Create base class for CosmosDb Connection

---
+++ OnlineStore/Repository/Base/CosmosDbConnectionBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using OnlineStore.Models.BaseModels;
+
+namespace OnlineStore.Repository.Base
+{
+    public class CosmosDbConnectionBase<TItem> : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly string _connectionString;
+
+        private readonly CosmosClient _cosmosClient;
+
+        public readonly string DocType;
+
+        private readonly string _containerName;
+
+        public readonly Container Container;
+
+        public CosmosDbConnectionBase(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("COSMOS_DB") ?? "N/A";
+
+            _cosmosClient = new CosmosClient(_connectionString);
+
+            Type type = typeof(TItem);
+            CosmosAttribute attribute = (CosmosAttribute)Attribute.GetCustomAttribute(type, typeof(CosmosAttribute));
+            DocType = attribute.DocType;
+            _containerName = attribute.Container;
+
+            Container = _cosmosClient.GetContainer(databaseId: "OnlineStore", containerId: _containerName);
+
+        }
+    }
+}
--- OnlineStore/Repository/BaseRepositoryController.cs
+++ OnlineStore/Repository/BaseRepositoryController.cs
@@ -2,49 +2,43 @@
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Linq;
 using OnlineStore.Models.BaseModels;
+using OnlineStore.Repository.Base;
 using System.Net;
 
 namespace OnlineStore.Repository
 {
-    public class BaseRepositoryController<Titem> : ControllerBase where Titem : BaseApiModel<Guid>
+    public class BaseRepositoryController<Titem> : CosmosDbConnectionBase<Titem> where Titem : BaseApiModel<Guid>
     {
-        private readonly IConfiguration _configuration;
-        private readonly string _connectionString;
-        private CosmosClient _cosmosClient;
-        private string _docType;
-        private string _containerName;
-        private Container _container;
-
-        public BaseRepositoryController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-            _connectionString = _configuration.GetConnectionString("COSMOS_DB") ?? "N/A";
-
-            _cosmosClient = new CosmosClient(_connectionString);
-
-            Type type = typeof(Titem);
-            CosmosAttribute attribute = (CosmosAttribute) Attribute.GetCustomAttribute(type, typeof(CosmosAttribute));
-            _docType = attribute.DocType;
-            _containerName = attribute.Container;
-
-            _container = _cosmosClient.GetContainer(databaseId: "OnlineStore", containerId: _containerName); 
-
-        }
+        public BaseRepositoryController(IConfiguration configuration) : base(configuration) { }
 
         [HttpGet("{id}")]
         public async Task<Titem> GetById(Guid id)
         {
-            IOrderedQueryable<Titem> queryable = _container.GetItemLinqQueryable<Titem>();
+            IOrderedQueryable<Titem> queryable = Container.GetItemLinqQueryable<Titem>();
             var result = queryable.Where(x => x.Id == id).ToFeedIterator();
             var response = await result.ReadNextAsync();
             return response.First();
+        }
+
+        [HttpGet("getall")]
+        public async Task<List<Titem>> GetAll()
+        {
+            var query = Container.GetItemLinqQueryable<Titem>().ToFeedIterator();
+            List<Titem> results = new();
+
+            while(query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+                results.AddRange(response.Resource);
+            }
+            return results;
         }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ApiResponseModel))]
         public async Task<ApiResponseModel> Create([FromBody]Titem model)
         {
-            var response = await _container.CreateItemAsync<Titem>(item: model, partitionKey: new PartitionKey(_docType));
+            var response = await Container.CreateItemAsync<Titem>(item: model, partitionKey: new PartitionKey(DocType));
             var result = new ApiResponseModel()
             {
                 StatuseCode = response.StatusCode,
